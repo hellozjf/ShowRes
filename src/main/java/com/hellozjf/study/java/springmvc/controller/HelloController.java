9673c9ad56b057a82452a119f715706006688445
hellozjf <908686171@qq.com>
2017-06-16 00:08:11 +0800

    Update

--- src/main/java/com/hellozjf/study/java/springmvc/controller/HelloController.java
+++ src/main/java/com/hellozjf/study/java/springmvc/controller/HelloController.java
@@ -1,22 +1,73 @@
 package com.hellozjf.study.java.springmvc.controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.hellozjf.study.java.springmvc.pojo.HelloPojo;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/hello.do", produces = "text/html;charset=UTF-8")
-	@ResponseBody
-	public String hello() {
-		HelloPojo helloPojo = new HelloPojo();
-		helloPojo.setId(1);
-		helloPojo.setName("hello");
-		
-		return JSONObject.toJSONString(helloPojo);
-	}
+    @RequestMapping(value = "/getRes.do", produces = "text/html;charset=UTF-8")
+    @ResponseBody
+    public String getRes() {
+
+        JSONObject ret = new JSONObject();
+        JSONArray xdata = new JSONArray();
+        JSONArray ydata = new JSONArray();
+
+        try {
+            File file = new File("d:/res.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            // InputStream in =
+            // this.getClass().getClassLoader().getResourceAsStream("res.txt");
+            // BufferedReader reader = new BufferedReader(new
+            // InputStreamReader(in));
+            List<JSONObject> jsonObjects = new ArrayList<JSONObject>();
+            String s = null;
+            while ((s = reader.readLine()) != null) {
+                JSONObject jsonObject;
+                try {
+                    jsonObject = JSONObject.parseObject(s);
+                } catch (Exception e) {
+                    continue;
+                }
+                if (jsonObject.containsKey("code")
+                        && jsonObject.containsKey("data")) {
+                    jsonObjects.add(jsonObject);
+                }
+            }
+
+            for (int i = jsonObjects.size() - 1; i >= 0; i--) {
+                JSONObject jsonObject = jsonObjects.get(i);
+                if (jsonObject.getIntValue("code") == 200) {
+                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+                    int size = jsonArray.size() <= 20 ? jsonArray.size() : 20;
+                    for (int j = 0; j < size; j++) {
+                        JSONArray array = jsonArray.getJSONArray(j);
+
+                        xdata.add(array.getString(0));
+                        ydata.add(array.getInteger(1));
+                    }
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ret.put("xdata", xdata);
+        ret.put("ydata", ydata);
+        return ret.toJSONString();
+    }
 }
